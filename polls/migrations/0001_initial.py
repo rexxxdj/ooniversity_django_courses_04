ae5698adfaf43659269e14d837220790d260af07
Ruslan Tokar <djrexxx87@gmail.com>
2016-04-06 02:07:41 +0300

    4 tutorial

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -11,21 +11,31 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Student',
+            name='Choice',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True)),
-                ('name', models.CharField(max_length=35)),
-                ('surname', models.CharField(max_length=35)),
-                ('patronymic', models.CharField(max_length=35)),
-                ('rating', models.IntegerField()),
-                ('package', models.CharField(max_length=35)),
-                ('birth_date', models.DateField()),
-                ('address', models.CharField(max_length=255)),
-                ('email', models.EmailField(max_length=75)),
-                ('skype', models.CharField(max_length=35)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice_text', models.CharField(max_length=200)),
+                ('votes', models.IntegerField(default=0)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question_text', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='choice',
+            name='question',
+            field=models.ForeignKey(to='polls.Question'),
+            preserve_default=True,
         ),
     ]
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,30 @@
-from django.db import models
+import datetime
 
-# Create your models here.
-class Student(models.Model):
-	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=35)
-	surname = models.CharField(max_length=35)
-	patronymic = models.CharField(max_length=35)
-	rating = models.IntegerField()
-	package = models.CharField(max_length=35)
-	birth_date = models.DateField()
-	address = models.CharField(max_length=255)
-	email = models.EmailField()
-	skype = models.CharField(max_length=35)
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):              # __unicode__ on Python 2
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'    
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):              # __unicode__ on Python 2
+        return self.choice_text
