07d851501f7f0f54d64fd10c3ba7f1ef9a77a44b
Ruslan Tokar <djrexxx87@gmail.com>
2016-04-05 22:49:15 +0300

    Django first step

---
+++ pybursa/pybursa/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def main(request):
+	return render(request,'index.html')
+
+def contact(request):
+	return render(request,'contact.html')
+
+def student_list(request):
+	return render(request, 'student_list.html')
+	
+def student_detail(request):
+	return render(request, 'student_detail.html')
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', main, name='home'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='contact'),
+    
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
