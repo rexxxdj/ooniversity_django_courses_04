46f8d8617e9e4abf36dcef13ed5288c1acf19393
Ruslan Tokar <djrexxx87@gmail.com>
2016-04-09 10:50:14 +0300

    add form

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -3,9 +3,6 @@ from django import forms
 
 
 class QuadraticForm(forms.Form):
-    """
-    Input variables of the quadratic equation
-    """
     a = forms.FloatField(label='Введите коэффициент a ', 
     					widget=forms.TextInput(attrs={'size': '10'}))
     b = forms.FloatField(label='Введите коэффициент b ', 
