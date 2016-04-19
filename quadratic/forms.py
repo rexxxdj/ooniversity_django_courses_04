ada211a4302993e5499961264ea1683c9dbe1afc
Ruslan Tokar <djrexxx87@gmail.com>
2016-04-20 00:44:34 +0300

    aaaaa

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 from django import forms
 
 class QuadraticForm(forms.Form):
-
-    a = forms.FloatField(label="коэффициент а")
-    b = forms.FloatField(label="коэффициент b")
-    c = forms.FloatField(label="коэффициент c")
+    a = forms.IntegerField(label="коэффициент a")
+    b = forms.IntegerField(label="коэффициент b")
+    c = forms.IntegerField(label="коэффициент c")
 
     def clean_a(self):
-        clean = self.cleaned_data['a']
-        if clean == 0:
+        data = self.cleaned_data['a']
+        if data == 0:
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        return self.cleaned_data['a']
+        return data
--- quadratic/views.py
+++ quadratic/views.py
@@ -1,54 +1,28 @@
-#-*-coding: utf-8-*-
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.template import RequestContext, loader
 from quadratic.forms import QuadraticForm
 
 def quadratic_results(request):
-    context = {}
+    numbers = {}
     if request.GET:
         form = QuadraticForm(request.GET)
         if form.is_valid():
-            b = form.cleaned_data['b']
             a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
             c = form.cleaned_data['c']
-
             d = b ** 2 - 4 * a * c
-
             if d < 0:
-                result_message = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+                result = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
             elif d == 0:
-                x = (-b + d**(1/2.0)) / 2*a
-                result_message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = {}" .format(x)
+                result = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %0.1f" % (-b / 2 * a)
             else:
-                x1 = (-b + d**(1/2.0)) / 2*a
-                x2 = (-b - d**(1/2.0)) / 2*a
-                result_message = "Квадратное уравнение имеет два действительных корня: x1 = {}, x2 = {}" .format(x1, x2)
-
-            context.update({'d': 'Дискриминант: %d' %d, 'result_message': result_message})
+                x1 = (-b + d ** (1/2.0)) / (2 * a)
+                x2 = (-b - d ** (1/2.0)) / (2 * a)
+                result = "Квадратное уравнение имеет два действительных корня: x1 = %0.1f, x2 = %0.1f" % (x1, x2)
+            numbers.update({ 'd' : d, 'result' : result })
     else:
         form = QuadraticForm()
-    text.update({ 'form' : form })
-    return render(request, "results.html",  text )
+    numbers.update({ 'form' : form })
 
-class Validation(object):
-
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
-        self.value_int = None
-        self.error_msg = None
-
-    def valid_quadratic(self):
-        if not self.value:
-            self.error_msg = 'коэффициент не определен'
-            return False
-        try:
-            self.value_int = int(self.value)
-        except ValueError:            
-            self.error_msg = 'коэффициент не целое число'
-            return False
-
-        if self.name == 'a' and self.value_int == 0:
-            self.error_msg = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-            return False
-        return True
-    return render(request, 'quadratic/results.html', {'form': form, 'context': context})
+    return render(request, 'results.html', numbers)
