3bc4ec6a7ab47fc2d8ca3eb4aa8c0e1f21d1e035
Ruslan Tokar <djrexxx87@gmail.com>
2016-04-20 23:07:57 +0300

    KZ 7-3 add-in

--- students/views.py
+++ students/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, redirect
 from students.models import Student
+from courses.models import Course
+from forms import StudentModelForm
+from django.contrib import messages
+from django.template import RequestContext
 
 def list_view(request):
     if request.GET:
@@ -11,3 +16,34 @@ def list_view(request):
 def detail(request, student_id):
     student = Student.objects.get(id=student_id)
     return render(request, 'students/detail.html', {'student': student})
+
+def create(request):
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST)
+        if form.is_valid():
+            student= form.save()
+            messages.success(request, u'Student %s %s has been successfully added.'%(student.name, student.surname))
+            return redirect('students:list_view')
+    else:
+    	form = StudentModelForm()
+    return render(request, 'students/add.html', {'form':form})
+
+def edit(request,id):
+    student_inst=Student.objects.get(pk=id)
+    if request.method == 'POST':
+        form = StudentModelForm(request.POST,instance=student_inst)
+        if form.is_valid():
+            student= form.save()
+            messages.success(request, u'Info on the student has been sucessfully changed.')
+            return redirect('students:list_view')                
+    else:
+        form = StudentModelForm(instance=student_inst)
+    return render(request,"students/edit.html",{"form":form})    
+
+def remove(request,id):
+    student=Student.objects.get(pk=id)
+    if request.method == 'POST':
+        messages.success(request, u'Info on %s %s has been sucessfully deleted.'%(student.name, student.surname))
+        student.delete()
+        return redirect('students:list_view')
+    return render(request,"students/remove.html",{"student":student})
