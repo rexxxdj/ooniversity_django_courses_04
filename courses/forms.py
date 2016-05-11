430ae05f4db49b717234ed7394e1e819beb6a0be
Ruslan Tokar <djrexxx87@gmail.com>
2016-05-11 22:49:55 +0300

    redesign close

--- courses/forms.py
+++ courses/forms.py
@@ -5,13 +5,15 @@ from courses.models import Course,Lesson
 class CourseModelForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = '__all__'
-        widgets = {
-			'name':forms.TextInput(attrs={'class':'form-control'}),
+        #widgets = {
+		#	'name':forms.TextInput(attrs={'class':'form-control'}),
 			#'short_description':forms.TextInput(attrs={'class':'form-control'}),
 			#'description':forms.TextInput(attrs={'class':'form-control'}),
 			#'message':forms.TextInput(attrs={'class':'form-control'}),
-		}
+		#}
+        #short_description = forms.CharField(widget=forms.Textarea)
+        #description = forms.CharField(widget=forms.Textarea)
+        fields = '__all__'
 
 class LessonModelForm(forms.ModelForm):
     class Meta:
--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -5,6 +5,13 @@ from feedbacks.models import Feedback
 class FeedbackForm(forms.ModelForm):    
 	class Meta:
 		model = Feedback
+		fields = '__all__'
+
+
+'''
+class FeedbackForm(forms.ModelForm):    
+	class Meta:
+		model = Feedback
 		widgets = {
 			'name':forms.TextInput(attrs={'class':'form-control'}),
 			'from_email':forms.TextInput(attrs={'class':'form-control'}),
@@ -12,4 +19,5 @@ class FeedbackForm(forms.ModelForm):
 			#'message':forms.TextInput(attrs={'class':'form-control'}),
 		}
 		message = forms.CharField(widget=forms.Textarea)
-		fields = '__all__'
+		fields = '__all__' 
+'''		
